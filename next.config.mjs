28184fa0217ffcbfb17377868144fefb65cb6691
edplayer2022-prog <edplayer2022@gmail.com>
2026-08-26 16:50:01 -0400

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+const repoName = '/marceli-e-mariani';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: repoName,
+  assetPrefix: repoName,
+  trailingSlash: true,
+  images: {unoptimized: true},
+};
+
+export default nextConfig;
+
